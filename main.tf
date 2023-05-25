4f7ac6e42c9cb97f23eb541d438e5b7557642971
akhil <akhileshreddy9181@gmail.com>
2023-05-25 17:34:17 +0530

    Added Exit Status to script

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_route_table" "public-route-table" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "aws_internet_gateway.igw.id"
+    gateway_id = aws_internet_gateway.igw.id
   }
   for_each = var.public_subnets
   tags = merge(
